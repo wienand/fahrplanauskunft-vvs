74a17f65d25bcf2cee099d8a9bfa3662742abdbe
Oliver Wienand <wienand@gmx.de>
2017-02-14 17:00:13 +0100

    added connection query

--- fahrplanauskunft-vvs.py
+++ fahrplanauskunft-vvs.py
@@ -18,7 +18,66 @@ def queryVVS():
     slots = {key.lower(): data['value'] for key, data in intent['slots'].items() if 'value' in data}
     if intent['name'] == 'GetDepartures':
         result = getDepartures(**slots)
+    elif intent['name'] == 'GetConnection':
+        result = getConnection(**slots)
     return json.dumps(result)
+
+
+def getConnection(source='Am Kriegsbergturm', target='Hauptbahnhof', time=None, date=None):
+    url = 'http://mobil.vvs.de/jqm/controller/XSLT_TRIP_REQUEST2?outputFormat=JSON&type_destination=stop&type_origin=stop&useRealtime=1&name_origin=%s&name_destination=%s' \
+          % (urllib.quote_plus(source.encode('utf8')), urllib.quote_plus(target.encode('utf8')))
+    if time:
+        url += '&itdTime=%s' % time.replace(':', '')
+    if date:
+        url += '&itdDate=%s' % date.replace('-', '')
+    response = urllib.urlopen(url)
+    response = json.loads(response.read().decode('latin1'))
+    trips = []
+    now = datetime.datetime.now()
+    for trip in response['trips'] or []:
+        hours, minutes = (int(x) for x in trip['duration'].split(':'))
+        tripText = 'Fahrzeit'
+        if hours:
+            tripText += ' %d Stunden und' % hours
+        tripText += ' %d Minuten' % minutes
+        interchanges = int(trip['interchange'])
+        if interchanges:
+            if interchanges == 1:
+                tripText += ' bei einem Umstieg'
+            else:
+                tripText += ' bei %d Umstiegen' % interchanges
+        firstLeg = True
+        for leg in trip['legs']:
+            if firstLeg:
+                firstLeg = False
+                dateTime = leg['points'][0]['dateTime']
+                departureTime = datetime.datetime.strptime(dateTime.get('rtDate', dateTime['date']) + ' ' + dateTime.get('rtTime', dateTime['time']), '%d.%m.%Y %H:%M')
+                minutesTillDeparture = (departureTime - now).total_seconds() / 60
+                if minutesTillDeparture < 0.5 or minutesTillDeparture > 60:
+                    tripText += ' mit %s in Richtung %s ab %s Uhr bis %s' % (leg['mode']['name'], leg['mode']['destination'].split(' (')[0], dateTime.get('rtTime', dateTime['time']), leg['points'][-1]['name'])
+                elif minutesTillDeparture > 0.5:
+                    tripText += ' mit %s in Richtung %s in %d Minuten bis %s' % (leg['mode']['name'], leg['mode']['destination'].split(' (')[0], minutesTillDeparture, leg['points'][-1]['name'])
+            else:
+                dateTime = leg['points'][0]['dateTime']
+                departureTime = datetime.datetime.strptime(dateTime.get('rtDate', dateTime['date']) + ' ' + dateTime.get('rtTime', dateTime['time']), '%d.%m.%Y %H:%M')
+                minutesForInterchange = (departureTime - lastArrivalTime).total_seconds() / 60
+                tripText += ', dann in %d Minuten umsteigen in %s Richtung %s bis %s' % (minutesForInterchange, leg['mode']['name'], leg['mode']['destination'].split(' (')[0], leg['points'][-1]['name'])
+            dateTime = leg['points'][-1]['dateTime']
+            lastArrivalTime = datetime.datetime.strptime(dateTime.get('rtDate', dateTime['date']) + ' ' + dateTime.get('rtTime', dateTime['time']), '%d.%m.%Y %H:%M')
+        trips.append(tripText)
+
+    result = {
+        "version"          : "1.0",
+        "sessionAttributes": {},
+        "response"         : {
+            "outputSpeech"    : {
+                "type": "PlainText",
+                "text": ". ".join(trips)
+            },
+            "shouldEndSession": True
+        }
+    }
+    return result
 
 
 def getDepartures(stop='Am Kriegsbergturm', time=None, date=None):
